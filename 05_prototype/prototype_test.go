7d606211c0dc23c5fbbe73290cdd74ae457b0de7
MagicGopher <gopher997@gmail.com>
2024-11-10 14:14:08 +0800

    fix: 修改原型模式单元测试使用的包路径。

--- 05_prototype/prototype_test.go
+++ 05_prototype/prototype_test.go
@@ -1,6 +1,7 @@
-package prototype
+package prototype_test
 
 import (
+	prototype "github.com/magicgopher/go-design-pattern/05_prototype"
 	"reflect"
 	"testing"
 )
@@ -8,7 +9,7 @@ import (
 // TestSheepClone 原型模式单元测试
 func TestSheepClone(t *testing.T) {
 	// 创建一个 Sheep 对象
-	original := &Sheep{name: "Dolly", weight: 45.5}
+	original := &prototype.Sheep{Name: "Dolly", Weight: 45.5}
 
 	// 克隆对象
 	clone := original.Clone()
@@ -19,10 +20,10 @@ func TestSheepClone(t *testing.T) {
 	}
 
 	// 验证克隆对象的属性是否与原始对象一致
-	if clone.(*Sheep).name != original.name || clone.(*Sheep).weight != original.weight {
+	if clone.(*prototype.Sheep).Name != original.Name || clone.(*prototype.Sheep).Weight != original.Weight {
 		t.Errorf("Expected name %s and weight %.2f, got name %s and weight %.2f",
-			original.name, original.weight,
-			clone.(*Sheep).name, clone.(*Sheep).weight)
+			original.Name, original.Weight,
+			clone.(*prototype.Sheep).Name, clone.(*prototype.Sheep).Weight)
 	}
 
 	// 验证克隆和原始对象是不同的实例
--- 05_prototype/sheep.go
+++ 05_prototype/sheep.go
@@ -8,19 +8,19 @@ import "fmt"
 
 // Sheep 羊
 type Sheep struct {
-	name   string  //名字
-	weight float64 // 体重
+	Name   string  //名字
+	Weight float64 // 体重
 }
 
 // Clone 返回一个新的 Sheep，复制当前对象
 func (s *Sheep) Clone() Prototype {
 	return &Sheep{
-		name:   s.name,
-		weight: s.weight,
+		Name:   s.Name,
+		Weight: s.Weight,
 	}
 }
 
 // String 输出 Sheep 的信息
 func (s *Sheep) String() string {
-	return fmt.Sprintf("Sheep{Name: %s, Weight: %0.2f}\n", s.name, s.weight)
+	return fmt.Sprintf("Sheep{Name: %s, Weight: %0.2f}\n", s.Name, s.Weight)
 }
